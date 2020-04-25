f2be9efccdb76b5325523e3b0745a4448594fdbd
Dennis Avilés Odar <dennisavilesodar@server.local>
2020-04-25 02:02:29 -0500

    Modifico principal

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <body>
     <h1>Hola Mundo cruel ... te saluda un Desarrollador ...</h1>
     <h2>Se desarrollara el proyecto Hoteles para el curso de Tecnologias Web</h2>
-    <h3>La primera en renegar sera Rut ... y dira ... esque eres muyyy intenso</h3>
-    <h4>En video llamda con Jesus</h4>
-   <h5>llueve mucho</h5> 
+    <h3>Ahora ya estoy hablando con jesus</h3>
+    <h5>llueve mucho</h5> 
+    <h4>Pongo un cambio adicional aqui</h4>
 </body>
 </html>
