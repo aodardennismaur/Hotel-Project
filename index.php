a64e7ceadaf9b530b56531c093d08c5b1bbf52a1
Dennis Avilés Odar <dennisavilesodar@server.local>
2020-04-25 00:33:58 -0500

    Actualizacion principal

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
     <h1>Hola Mundo cruel ... te saluda un Desarrollador ...</h1>
     <h2>Se desarrollara el proyecto Hoteles para el curso de Tecnologias Web</h2>
     <h3>La primera en renegar sera Rut ... y dira ... esque eres muyyy intenso</h3>
+    <h4>En video llamda con Jesus</h4>
 </body>
 </html>
