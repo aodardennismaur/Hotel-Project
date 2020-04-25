7559138cb5194c5556e34e5268b721b5bf3861c7
Dennis Avilés Odar <dennisavilesodar@server.local>
2020-04-24 23:37:05 -0500

    Primer commit

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <head>
 </head>
 <body>
-    <h1>Hola Mundo cruel ... te saluda un Desarrollador ...</h1>    
+    <h1>Hola Mundo cruel ... te saluda un Desarrollador ...</h1>
+    <h2>Se desarrollara el proyecto Hoteles para el curso de Tecnologias Web</h2>
+    <h3>La primera en renegar sera Rut ... y dira ... esque eres muyyy intenso</h3>
 </body>
 </html>
