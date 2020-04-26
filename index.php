e6c0898b1442bba05115fe68d11e91e2a68800d4
Dennis Avilés Odar <dennisavilesodar@server.local>
2020-04-26 04:50:49 -0500

    Prueba de rendimiento

--- index.php
+++ index.php
@@ -3,9 +3,5 @@
 </head>
 <body>
     <h1>Hola Mundo cruel ... te saluda un Desarrollador ...</h1>
-    <h2>Se desarrollara el proyecto Hoteles para el curso de Tecnologias Web</h2>
-    <h3>Ahora ya estoy hablando con jesus</h3>
-    <h5>llueve mucho</h5> 
-    <h4>Pongo un cambio adicional aqui</h4>
 </body>
 </html>
