15ac39c80c99074196435d1c827b1d2af7ac91e6
Dennis aviles <aodardennismaur@crece.uss.edu.pe>
2020-05-25 23:05:12 -0500

    Arreglos fix

--- assets/js/accion.js
+++ assets/js/accion.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+    $('#fechaEntrada').datetimepicker({
+        timepicker: false,
+        format: 'd/m/Y',
+        minDate: 0
+    });
+    $('#fechaSalida').datetimepicker({
+        timepicker: false,
+        format: 'd/m/Y',
+        onShow: function () {
+            this.setOptions({
+                minDate: $('#fechaEntrada').val()
+            })
+        }
+    });
 });
