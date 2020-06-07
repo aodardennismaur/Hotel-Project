075ac1fbf725707f242b97e956374fa6d5ad4898
Dennis aviles <aodardennismaur@crece.uss.edu.pe>
2020-06-07 03:23:57 -0500

    Login

--- assets/js/accion.js
+++ assets/js/accion.js
@@ -13,60 +13,123 @@ $(document).ready(function () {
     //         })
     //     }
     // });
-    $('#login').submit(function (e) {
-        e.preventDefault();
-        var correo = $('#correo').val();
-        var clave = $('#clave').val();
+});
 
-        if (correo === "" || clave === "") {
-            swal({
-                title: 'Error',
-                text: 'Alguno de los campos se encuentra vacio',
-                icon: 'error',
-                buttons: false,
-                timer: 2000
-            });
-            var vacio = correo === "" ? "#correo" : "#clave";
-            $(vacio).focus();
-            return;
-        }
+$('#registro').submit(function (e) {
+    e.preventDefault();
+    var correo = $('#correoRegistro').val();
+    var clave = $('#claveRegistro').val();
 
-        if (validateEmail(correo)) {
-            $.ajax({
-                type: "POST",
-                url: '../controller/sesion.controller.php',
-                data: $(this).serialize(),
-                success: function (data) {
-                    swal({
-                        title: 'Existo',
-                        text: 'Bienvenido al sistema: ' + data.usuario,
-                        icon: 'success',
-                        button: 'OK'
-                    });
-                    console.info(data);
-                },
-                error: function (er) {
-                    swal({
-                        title: 'Error',
-                        text: 'error con el servidor: ' + er.status,
-                        icon: 'error',
-                        button: 'OK'
-                    });
-                    console.info(er);
-                    return;
-                }
-            });
-        } else {
-            swal({
-                title: 'Error',
-                text: 'El formato del correo ingresado no es valido',
-                icon: 'error',
-                buttons: false,
-                timer: 2000
-            });
-            return;
-        }
-    });
+    if (correo === "" || clave === "") {
+        swal({
+            title: 'Error',
+            text: 'Alguno de los campos se encuentra vacio',
+            icon: 'error',
+            buttons: false,
+            timer: 2000
+        });
+        var vacio = correo === "" ? "#correo" : "#clave";
+        $(vacio).focus();
+        return;
+    }
+
+    if (validateEmail(correo)) {
+        $.ajax({
+            type: "POST",
+            url: '../controller/registro.controller.php',
+            data: $(this).serialize(),
+            success: function (data) {
+                swal({
+                    title: 'Existo',
+                    text: data,
+                    icon: 'success',
+                    button: 'OK'
+                }).then(function () {
+                    $('#correoRegistro').val('');
+                    $('#claveRegistro').val('');
+                    window.location = '../views/login.html';
+                });
+            },
+            error: function (er) {
+                swal({
+                    title: 'Error',
+                    text: 'error con el servidor: ' + er.status,
+                    icon: 'error',
+                    button: 'OK'
+                });
+                console.info(er);
+                return;
+            }
+        });
+    } else {
+        swal({
+            title: 'Error',
+            text: 'El formato del correo ingresado no es valido',
+            icon: 'error',
+            buttons: false,
+            timer: 2000
+        });
+        return;
+    }
+});
+
+$('#register').on('click', function (e) {
+    window.location = "../views/registrar.html";
+});
+
+$('#login').submit(function (e) {
+    e.preventDefault();
+    var correo = $('#correo').val();
+    var clave = $('#clave').val();
+
+    if (correo === "" || clave === "") {
+        swal({
+            title: 'Error',
+            text: 'Alguno de los campos se encuentra vacio',
+            icon: 'error',
+            buttons: false,
+            timer: 2000
+        });
+        var vacio = correo === "" ? "#correo" : "#clave";
+        $(vacio).focus();
+        return;
+    }
+
+    if (validateEmail(correo)) {
+        $.ajax({
+            type: "POST",
+            url: '../controller/sesion.controller.php',
+            data: $(this).serialize(),
+            success: function (data) {
+                swal({
+                    title: 'Existo',
+                    text: 'Bienvenido al sistema: ' + data.usuario,
+                    icon: 'success',
+                    button: 'OK'
+                });
+                console.info(data);
+            },
+            error: function (er) {
+                swal({
+                    title: 'Error',
+                    text: 'error con el servidor: ' + er.status,
+                    icon: 'error',
+                    button: 'OK'
+                });
+                console.info(er);
+                return;
+            }
+        });
+    } else {
+        swal({
+            title: 'Error',
+            text: 'El formato del correo ingresado no es valido',
+            icon: 'error',
+            buttons: false,
+            timer: 2000
+        });
+        return;
+    }
 });
 
 
